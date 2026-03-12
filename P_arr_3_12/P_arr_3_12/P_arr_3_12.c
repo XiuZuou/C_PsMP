42cbfb45cdddfdf84353057616af014fd5304738
xiuzuou <3146114514@qq.com>
2026-03-12 21:23:33 +0800

    2026/3/12一次矩阵转置练习

---
+++ P_arr_3_12/P_arr_3_12/P_arr_3_12.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int m = 0;
+	int i = 0;
+	int j = 0;
+	int arr[10][10] = { 0 };
+	while (scanf("%d %d", &n, &m) != EOF)
+	{
+		for (i = 0;i < n; i++)
+		{
+			for (j = 0;j < m;j++)
+				scanf("%d", &arr[i][j]);
+		}
+		for (i = 0;i < m;i++)
+		{
+			for (j = 0;j < n;j++)
+				printf("%d ", arr[j][i]);
+			printf("\n");
+
+		}
+	}
+
+	return 0;
+}
