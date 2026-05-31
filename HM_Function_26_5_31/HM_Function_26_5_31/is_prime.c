65c459e61cf5a93dcbf5e9c2d176764ff17b1c63
xiuzuou <xiuzu.ou@gmail.com>
2026-05-31 22:03:11 +0800

    26_5_31用函数判断是否是素数并输出100~200间的素数

---
+++ HM_Function_26_5_31/HM_Function_26_5_31/is_prime.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int is_prime(int x)
+
+{
+
+}
+
+int main()
+
+{
+	int i = 0;
+	for (i = 100;i <= 200;i++)
+	{
+		int j = 0;
+		for (j = 2;j < i;j++)
+		{
+			if (i % j == 0)
+			{
+				break;
+			}
+			
+		}
+		if (j == i)
+			printf("%d ", i);
+	}
+
+	return 0;
+}
