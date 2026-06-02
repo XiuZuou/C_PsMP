6a71df8019e19bfbfc747e17309d11b188684719
xiuzuou <xiuzu.ou@gmail.com>
2026-06-02 20:12:16 +0800

    26_6_2使用函数判断是否为素数并输出（未进行优化）

--- HM_Function_26_5_31/HM_Function_26_5_31/is_prime.c
+++ HM_Function_26_5_31/HM_Function_26_5_31/is_prime.c
@@ -5,7 +5,16 @@
 int is_prime(int x)
 
 {
-
+	int i = 0;
+	for ( i = 2;i < x;i++)
+	{
+		if (x % i == 0)
+		break;
+	}
+	if (x == i)
+		return x;
+	else
+		return -1;
 }
 
 int main()
@@ -14,18 +23,31 @@ int main()
 	int i = 0;
 	for (i = 100;i <= 200;i++)
 	{
-		int j = 0;
-		for (j = 2;j < i;j++)
-		{
-			if (i % j == 0)
-			{
-				break;
-			}
-			
-		}
-		if (j == i)
-			printf("%d ", i);
+		int pri = 0;
+		pri = is_prime(i);
+		if (pri != -1)
+			printf("%d ", pri);
 	}
 
 	return 0;
 }
+
+//int main()
+//{
+//	int i = 0;
+//	
+//	for (i = 100;i <= 200;i++)
+//	{
+//		int j = 0;
+//		for (j = 2;j < i;j++)
+//		{
+//			if (i % j == 0)
+//			break;
+//		}
+//		if (i == j)
+//		{
+//			printf("%d ", i);
+//		}
+//	}
+//	return 0;
+//}
