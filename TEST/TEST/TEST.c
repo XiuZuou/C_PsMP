ff8955a0a76a95aef5b21599d9e8d0bd395ae94e
xiuzuou <xiuzu.ou@gmail.com>
2026-06-03 23:21:52 +0800

    用于测试上传

---
+++ TEST/TEST/TEST.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+	int money = 20;//题目要求20元买汽水
+	int bottles = 0;
+	int soda = 0;
+	int change = 0;
+	//int B_change = 0;
+	int sum = 0;
+	soda = money;//汽水1元/瓶
+	bottles = soda;
+	while (bottles >= 2)//瓶子数量大于等于2就可以继续兑换汽水
+	{
+		bottles = soda;
+		change = bottles / 2;
+		sum += soda;
+		soda = change;
+
+	}
+	printf("20元可以喝的汽水数量为：%d", sum);
+
+	return 0;
+}
