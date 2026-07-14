cfa6ce414535298a66535d5f3bc5217c34262458
xiuzuou <xiuzu.ou@gmail.com>
2026-07-14 18:02:59 +0800

    26/7/14 从新开始

---
+++ Restart_26_7_14/Restart_26_7_14/RAC.c
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"RAC.h"
+void my_ambition()
+{
+	printf("Restart and consist!\n");
+	return;
+}
---
+++ Restart_26_7_14/Restart_26_7_14/Restart anf Consist.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"RAC.h"
+
+int main()
+{
+
+	my_ambition();
+
+	return 0;
+}
